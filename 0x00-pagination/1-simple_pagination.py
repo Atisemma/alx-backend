1dd47068fbfb7e9c8b54ceec5cb94f84fe65fd2f
Atisemma <atisemma26@gmail.com>
2024-08-08 15:48:08 -0700

    Simple pagination

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+"""
+This module contains the `index_range` function, which takes two integer arguments
+`page` and `page_size` and returns a tuple of size two containing a start index
+and an end index corresponding to the range of indexes to return in a list for
+those particular pagination parameters.
+"""
+
+import csv
+import math
+from typing import List, Tuple
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Returns the start and end indices for the given page and page size.
+
+    Args:
+        page (int): The page number (1-indexed).
+        page_size (int): The number of items per page.
+
+    Returns:
+        Tuple[int, int]: A tuple containing the start and end indices for the
+        given page and page size.
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Returns the appropriate page of the dataset.
+
+        Args:
+            page (int, optional): The page number (1-indexed). Defaults to 1.
+            page_size (int, optional): The number of items per page. Defaults to 10.
+
+        Raises:
+            AssertionError: If `page` or `page_size` are not integers greater than 0.
+
+        Returns:
+            List[List]: The appropriate page of the dataset.
+        """
+        assert isinstance(page, int) and page > 0, "page must be a positive integer"
+        assert isinstance(page_size, int) and page_size > 0, "page_size must be a positive integer"
+
+        dataset = self.dataset()
+        start_index, end_index = index_range(page, page_size)
+
+        if start_index >= len(dataset):
+            return []
+        else:
+            return dataset[start_index:end_index]
